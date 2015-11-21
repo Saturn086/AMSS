c9a3049d3d86be236a2b20bf9d5f776d1e64c98b
Carlos Googles <carlitox_garza@hotmail.com>
2015-11-20 18:05:40 -0600

    Agregar metodos para la entidad ubicacion y asesoria, y al controlador actualizador de MAEs disponibles, para el caso de uso Especificar ubicacion.

--- MAES_system/Controles/ControlActualizarMAESDisponibles.java
+++ MAES_system/Controles/ControlActualizarMAESDisponibles.java
@@ -9,48 +9,72 @@ import java.util.*;
 
 public class ControlActualizarMAESDisponibles {
 	Usuario usuario;
-  Materia materia;
-  Asesoria asesoria;
-  Asistencia asistencia;
-  Ubicacion ubicacion;
-  ArrayList<RowAsesoria> rowAsesoriaList;
-  ArrayList<String> matriculasList;
-  String strResultado;
+	Materia materia;
+	Asesoria asesoria;
+	Asistencia asistencia;
+	Ubicacion ubicacion;
+	ArrayList<RowAsesoria> rowAsesoriaList;
+	ArrayList<String> matriculasList;
+	String strResultado;
 
 	public ControlActualizarMAESDisponibles() {
-		  //Asume que la instancia persiste durante la sesion
-    	usuario = new Usuario();
-      materia = new Materia();
-      asesoria = new Asesoria();
-      asistencia = new Asistencia();
-      ubicacion = new Ubicacion();
-      rowAsesoriaList = new ArrayList<RowAsesoria>();
-      matriculasList = new ArrayList<String>();
-      strResultado = "";
+		//Asume que la instancia persiste durante la sesion
+		usuario = new Usuario();
+		materia = new Materia();
+		asesoria = new Asesoria();
+		asistencia = new Asistencia();
+		ubicacion = new Ubicacion();
+		rowAsesoriaList = new ArrayList<RowAsesoria>();
+		matriculasList = new ArrayList<String>();
+		strResultado = "";
 	}
 
-	//Obtener todas las asesorías dadas en el momento de la consulta
+	//Obtener todas las asesorias dadas en el momento de la consulta
 	public String mostrarAsesorias() {
-	    matriculasList = asistencia.obtenerMatriculasActivas();
+		matriculasList = asistencia.obtenerMatriculasActivas();
 
-	    for(String strMatricula : matriculasList) {
-	      rowAsesoriaList = asesoria.obtenerAsesoria(strMatricula);
+		for(String strMatricula : matriculasList) {
+			rowAsesoriaList = asesoria.obtenerAsesoria(strMatricula);
 
 				for(RowAsesoria rowAsesoria : rowAsesoriaList) {
-			      String strNombre = usuario.obtenerNombre(rowAsesoria.getStrMatriculaMAE());
-			      String strCantAlumnos = Integer.toString(rowAsesoria.getICantAlumnos());
-			      String strNombreMateria = materia.strObtenerNombreMateria(rowAsesoria.getIMateria());
-			      String strDisponibilidad = Character.toString(rowAsesoria.getCDisponibilidad());
-			      String strUbicacion = ubicacion.strObtenerNombre(rowAsesoria.getILugar());
+					String strNombre = usuario.obtenerNombre(rowAsesoria.getStrMatriculaMAE());
+					String strCantAlumnos = Integer.toString(rowAsesoria.getICantAlumnos());
+					String strNombreMateria = materia.strObtenerNombreMateria(rowAsesoria.getIMateria());
+					String strDisponibilidad = Character.toString(rowAsesoria.getCDisponibilidad());
+					String strUbicacion = ubicacion.strObtenerNombre(rowAsesoria.getILugar());
 
-			      strResultado += strNombre + " - " + strCantAlumnos + " - " + strNombreMateria + " - ";
-						if(strDisponibilidad.equals("0")) { strResultado += "No"; }
-						else {strResultado += "Si"; }
-
-						strResultado += " - " + strUbicacion + "<br>";
+					strResultado += strNombre + " - " + strCantAlumnos + " - " + strNombreMateria + " - ";
+					if(strDisponibilidad.equals("0")) {
+						strResultado += "No";
+					}
+					else {
+						strResultado += "Si";
 					}
 
-	    }
+					strResultado += " - " + strUbicacion + "<br>";
+				}
+		}
 		return strResultado;
+	}
+	
+	// Mostrar los MAEs que se encuentran disponibles.
+	public ArrayList<String> desplegarMaesDisp() {
+		ArrayList<String> lstStrNombres = new ArrayList<String>();
+		int intCantUbicacion = ubicacion.intObtenerCantidadUbicaciones();
+
+		for(int i = 1; i <= intCantUbicacion; i++) {
+			String strUbicacion = ubicacion.strObtenerNombre(i);
+			
+			lstStrNombres.add(i + "");
+			lstStrNombres.add(strUbicacion);
+		}
+		
+		return lstStrNombres;
+	}
+	
+	
+	// Mostrar los MAEs que se encuentran disponibles.
+	public void asginarUbicacion(String strMatricula) {
+		asesoria.asignarUbicacion(strMatricula);
 	}
 }
--- MAES_system/Entidades/Asesoria.java
+++ MAES_system/Entidades/Asesoria.java
@@ -5,50 +5,69 @@ import java.lang.String;
 import java.util.*;
 
 public class Asesoria {
-   Connection conn;
-   Statement stmt;
+	Connection conn;
+	Statement stmt;
 
-   public Asesoria() {
-      try {
-        String userName = "root";
-        String password = "";
-        String url = "jdbc:mysql://localhost/maes_system";
-        Class.forName ("com.mysql.jdbc.Driver").newInstance();
-        conn = DriverManager.getConnection (url, userName, password);
-        stmt = conn.createStatement();
-      } catch (Exception e) { System.out.println ("Cannot connect to database" +
-          "server"); }
-   }
+	public Asesoria() {
+		try {
+			String userName = "root";
+			String password = "";
+			String url = "jdbc:mysql://localhost/maes_system";
+			Class.forName ("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection (url, userName, password);
+			stmt = conn.createStatement();
+		} 
+		catch (Exception e) {
+			System.out.println("Cannot connect to database server"); 
+		}
+	}
 
-   public ArrayList<RowAsesoria> obtenerAsesoria(String strMatricula) {
-        try {
-          //Crea la instancia que será regresada por el método
-          RowAsesoria rowAsesoria;
-          ArrayList<RowAsesoria> rowAsesoriaList = new ArrayList<RowAsesoria>();
+	public ArrayList<RowAsesoria> obtenerAsesoria(String strMatricula) {
+		try {
+			//Crea la instancia que será regresada por el método
+			RowAsesoria rowAsesoria;
+			ArrayList<RowAsesoria> rowAsesoriaList = new ArrayList<RowAsesoria>();
 
-           stmt.executeQuery ("SELECT *, COUNT(*) AS cantAlumnos FROM Asesoria"
-              + " WHERE matricula_mae = " + "\'" + strMatricula + "\'" +
-              " GROUP BY matricula_mae, materia, lugar");
-           ResultSet rs = stmt.getResultSet();
-           while (rs.next()) { //Va al primer registro si lo hay
-             //Crea a la instancia que será regresada como resultado del query
-            rowAsesoria = new RowAsesoria();
-            rowAsesoria.setStrMatriculaMAE(rs.getString("matricula_mae"));
-            rowAsesoria.setILugar(rs.getInt("lugar"));
-            rowAsesoria.setIMateria(rs.getInt("materia"));
-            rowAsesoria.setICantAlumnos(rs.getInt("cantAlumnos"));
-            rowAsesoria.setCDisponibilidad(rs.getString("disponibilidad").charAt(0));
-            rowAsesoriaList.add(rowAsesoria);
-
-
-        }
-        rs.close();
-        return rowAsesoriaList;
-        } catch (SQLException e) {System.out.println("Error en obtenerAsesoria"+
-            " dentro de Asesoria."); }
-        return null;
-    }
-
+			stmt.executeQuery ("SELECT *, COUNT(*) AS cantAlumnos FROM Asesoria" +
+				" WHERE matricula_mae = " + "\'" + strMatricula + "\'" +
+				" GROUP BY matricula_mae, materia, lugar");
+			ResultSet rs = stmt.getResultSet();
+			
+			while (rs.next()) { //Va al primer registro si lo hay
+				//Crea a la instancia que será regresada como resultado del query
+				rowAsesoria = new RowAsesoria();
+				rowAsesoria.setStrMatriculaMAE(rs.getString("matricula_mae"));
+				rowAsesoria.setILugar(rs.getInt("lugar"));
+				rowAsesoria.setIMateria(rs.getInt("materia"));
+				rowAsesoria.setICantAlumnos(rs.getInt("cantAlumnos"));
+				rowAsesoria.setCDisponibilidad(rs.getString("disponibilidad").charAt(0));
+				rowAsesoriaList.add(rowAsesoria);
+			}
+			
+			rs.close();
+			return rowAsesoriaList;
+		} 
+		catch (SQLException e) {
+			System.out.println("Error en obtenerAsesoria" + " dentro de Asesoria.");
+		}
+		
+		return null;
+	}
+	
+	
+	public void asignarUbicacion(String strMatricula) {
+		try {
+			stmt.executeQuery(
+				"ALTER Asesoria" +
+				"WHERE matricula_mae = " + "\'" + strMatricula + "\'" +
+				"MODIFY COLUMN lugar INT"
+				);
+		} 
+		catch (SQLException e) {
+			System.out.println("Error en subAsignarUbicacion" + " dentro de Asesoria.");
+		}
+	}
+	
     // ####################eliminarAsesoria no está completamente implementada #######################################
     /*public void eliminarAsesoria(String strMatricula, int iMateria) {
       try {
