f163ee99306fbec6d47a9db282d9a0cb76ebe977
Pando65 <omjrrz@outlook.com>
2015-11-20 18:34:25 -0600

    add registrar ubicacion interface

--- MAES_system/Interfaces/InterfazInicioSesion.java
+++ MAES_system/Interfaces/InterfazInicioSesion.java
@@ -86,10 +86,11 @@ public class InterfazInicioSesion extends HttpServlet {
 		boolean boolExistente = cvVerifi.verificarCrendenciales(strMatricula, strContrasena);
 		if (boolExistente) {
 			out.println("<p>¡¡¡Bienvenido " + strMatricula + "!!!</p>");
-			out.println("<a href=\"mostrarMaes\" type=\"button\" class=\"btn btn-primary\"> Lista de MAES Disponibles </a> <br>");
+			out.println("<a href=\"registrarUbicacion\" type=\"button\" class=\"btn btn-primary\"> Registar mi Ubicacion </a> <br>");
 			//ligas a funciones de administradores
 			if(strMatricula.equals("A00000000")) {
 				out.println("<br> <p> Funciones de administrador </p>");
+				out.println("<a href=\"mostrarMaes\" type=\"button\" class=\"btn btn-primary\"> Lista de MAES Disponibles </a> <br> <br>");
 				out.println("<a href=\"mostrarAsistencia\" type=\"button\" class=\"btn btn-primary\"> Lista de Asistencia de MAES </a> <br>");
 			}
 		} else {
---
+++ MAES_system/Interfaces/InterfazRegistrarUbicacion.java
@@ -0,0 +1,57 @@
+package interfaces;
+import controles.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import controles.ControlVerificador;
+
+public class InterfazRegistrarUbicacion extends HttpServlet {
+	HttpServletResponse thisResponse;
+	HttpServletRequest thisRequest;
+	PrintWriter out;
+	//declarar controladores
+
+	//Es importante observar que todos los metodos definen la operacion GET para
+	//	que el metodo doGet sea el que se ejecuta al presionar el boton "Enviar".
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+		thisResponse = response;
+		thisRequest = request;
+		thisResponse.setContentType("text/html");
+
+		out = thisResponse.getWriter();
+		//Preparar el encabezado de la pagina Web de respuesta
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("<HEAD>");
+		out.println("<META http-equiv=Content-Type content=\"text/html\">");
+		out.println("<meta charset=\"UTF-8\">");
+		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+		out.println("<link href=\"css/bootstrap.min.css\" rel=\"stylesheet\">");
+		out.println("</HEAD>");
+		out.println("<BODY>");
+		out.println("<br> <br> <br>");
+		out.println("<div class=\"container text-center\">");
+		out.println("<div class=\"panel panel-primary\">");
+		out.println("<div class=\"panel-heading\">");
+		out.println("<H1 class=\"panel-title\">Mentores Académicos de Excelencia</H1>");
+		out.println("</div>");
+		out.println("<div class=\"panel-body\">");
+		out.println("<h3>Seleccion de Ubicacion</h3> <br>");
+
+		String strOperacion = request.getParameter("operacion");
+		// El menu nos envia un parametro para indicar el inicio de sesion
+		if (strOperacion == null) {
+			mostrarSeleccion();
+		}
+
+		out.println("</BODY>");
+		out.println("</HTML>");
+	}
+
+	public void mostrarSeleccion() {
+		out.println("Seleccion de Ubicacion");
+	}
+
+}
